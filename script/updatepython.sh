774dca22b3f4dcc41afc8f0a80aa05aa2a8c806e
schreinerman <manuel@schreiner-home.info>
2023-08-02 10:51:48 +0200

    Update updatepython.sh for several python versions

--- script/updatepython.sh
+++ script/updatepython.sh
@@ -2,29 +2,32 @@
 source utils.sh
 source fixpermissions.sh
 
+updatePythonVersion()
+{
+    PYTON_VERSION=$1
+    PIP="pip$PYTON_VERSION"
+    PYTHON="python$PYTON_VERSION"
+    if [ "$(command -v $PIP)" ]
+    then
+        echo "${GREEN}Updating python $PYTON_VERSION packages...${NC}"
+        fixPermissions
+        $PYTHON -m pip uninstall -y pip
+        $PYTHON -m pip install --user --upgrade pip
+        
+        $PIP list -o | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 $PIP install --upgrade --no-cache-dir --use-deprecated=legacy-resolver || true
+    fi
+}
+
 updatePython()
 {
     #update python packages
-    if [ "$(command -v pip3)" ]
-    then
-        echo "${GREEN}Updating python 3 packages...${NC}"
-        fixPermissions
-        pip3 list -o | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip3 install --upgrade --no-cache-dir --use-deprecated=legacy-resolver || true
-    fi
-
-    if [ "$(command -v python2)" ]
-    then
-        echo "${GREEN}Updating python 2 packages...${NC}"
-        fixPermissions
-        python2 -m pip list -o | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 python2 -m pip install --upgrade --no-cache-dir --use-deprecated=legacy-resolver  || true
-    fi
-
-    if [ "$(command -v pip)" ]
-    then
-        echo "${GREEN}Updating python packages...${NC}"
-        fixPermissions
-        pip list -o | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade --no-cache-dir --use-deprecated=legacy-resolver  || true
-    fi
-
+    updatePythonVersion "2"
+    updatePythonVersion ""
+    updatePythonVersion "3.11"
+    updatePythonVersion "3.10"
+    updatePythonVersion "3.9"
+    updatePythonVersion "3.8"
+    updatePythonVersion "3.7"
+    updatePythonVersion "3.6"
     fixPermissions
 }
