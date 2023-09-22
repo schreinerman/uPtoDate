1dfa5405caa77b4eda56666b460ccae2295a0218
schreinerman <manuel@schreiner-home.info>
2023-09-22 10:08:46 +0200

    Ensure pip

--- script/updatepython.sh
+++ script/updatepython.sh
@@ -5,16 +5,20 @@ source fixpermissions.sh
 updatePythonVersion()
 {
     PYTON_VERSION=$1
-    PIP="pip$PYTON_VERSION"
+    PIP="$PYTHON -m pip"
+    if [ "$(command -v "pip$PYTON_VERSION")" ]
+    then
+        PIP="$(which pip$PYTON_VERSION)"
+    fi
     PYTHON="python$PYTON_VERSION"
     if [ "$(command -v $PIP)" ]
     then
         printf "${GREEN}Updating python $PYTON_VERSION packages...${NC}"
         fixPermissions
-        $PYTHON -m pip uninstall -y pip
-        $PYTHON -m pip install --user --upgrade pip
-        $PYTHON -m pip install --upgrade --force-reinstall pip
-        
+        $PYTHON -m pip uninstall -y pip || true
+        $PYTHON -m pip install --user --upgrade pip || true
+        $PYTHON -m pip install --upgrade --force-reinstall pip || true
+        $PYTHON -m ensurepip --upgrade
         $PIP list -o | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 $PIP install --upgrade --no-cache-dir --use-deprecated=legacy-resolver || true
     fi
 }
